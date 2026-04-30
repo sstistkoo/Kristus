e884ec25a59e12e25c666b11ee828c8c777f3eb6
stistko <65913642+sstistkoo@users.noreply.github.com>
2026-04-30 17:40:12 +0200

    Revert "[23.05.2024 14:30] opravne překlady temat .chbyi sekvenčni překlad a přepis jenom vybranych , ted přepisuje myslim všechno"
    This reverts commit cd3847fe9b3b36c3c46db20f10ae8ecc97414aca.

--- strong_translate/fix.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let d = fs.readFileSync('js/translation/topicRepair.js', 'utf8');
-const search = "} = deps;\r\nfunction getTopicSourceTextForPreview(key, topicId) {";
-const replace = "} = deps;\r\n\r\nconst FALLBACK_TOPIC_ORDER = ['definice', 'vyznam', 'kjv', 'pouziti', 'puvod', 'specialista'];\r\n\r\nfunction getFailedTopicsForFallbackFn(translationEntry) {\r\n  const t = translationEntry || {};\r\n  const failed = [];\r\n  for (const topicId of FALLBACK_TOPIC_ORDER) {\r\n    const val = String(t[topicId] || '').trim();\r\n    if (!hasMeaningfulValue(val)) {\r\n      failed.push(topicId);\r\n      continue;\r\n    }\r\n    if (topicId === 'definice' && isDefinitionLowQuality(val)) {\r\n      failed.push(topicId);\r\n    }\r\n  }\r\n  return failed;\r\n}\r\n\r\nfunction getTopicSourceTextForPreview(key, topicId) {";
-let d2 = d.replace(search, replace);
-fs.writeFileSync('js/translation/topicRepair.js', d2, 'utf8');
-console.log('OK - replaced');
