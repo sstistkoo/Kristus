888ac549db02f8aa982cd9c27a475dabd690f5d7
stistko <65913642+sstistkoo@users.noreply.github.com>
2026-05-12 05:27:00 +0200

    [24.05.2024 21:55] Aktualizace i18n: přidání překladů pro němčinu, slovenštinu a polštinu a rozšíření seznamu podporovaných jazyků v JS

--- strong_translate/js/i18n.js
+++ strong_translate/js/i18n.js
@@ -4,7 +4,7 @@ export const UI_LANG_KEY = 'strong_ui_lang';
 export const DEFAULT_UI_LANG = 'cs';
 // Pro úsporu údržby jsou aktivní pouze cs a en.
 // Ostatní jazyky jsou prázdné (soubory zůstávají pro budoucí rozšíření).
-export const UI_LANGS = new Set(['cs', 'en']);
+export const UI_LANGS = new Set(['cs', 'en', 'de', 'sk', 'pl']);
 const FIXED_EN_KEYS = new Set([
   'detail.label.definitionEn',
   'export.field.definitionEn'
